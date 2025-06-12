210b0329602f8f445f31cdb776107091abae3a84
Gaurav Rai <rai2826@gmail.com>
2025-06-12 23:13:18 +1000

    strings

---
+++ Basics/string.js
@@ -0,0 +1,7 @@
+const name = "Gaurav Rai";
+const age = 37;
+const address = "9 burrora way Craigieburn";
+
+console.log(
+  `hello my name is ${name} and my age is ${age} and my address is ${address}`
+);
--- Basics/variable.js
+++ Basics/variable.js
@@ -1,1 +1,9 @@
 const a = 10;
+
+console.log(a);
+
+// constant cant be changed , changing a constant would give an error
+
+a = 20;
+
+console.log(a);
