1d4a6b447ef1ecf3e17be99a3a2104f12c13ee1a
Gaurav Rai <rai2826@gmail.com>
2025-06-15 17:46:31 +1000

    random function for match

--- Basics/numsAndMaths.js
+++ Basics/numsAndMaths.js
@@ -1,2 +1,10 @@
 const score = 40000;
 console.log(score.toLocaleString());
+
+//const x = (1, 2, 3, 5, 9);
+
+console.log(Math.min(10, 3, 6, 9));
+console.log(Math.max(45, 67, 0, 100));
+
+//Use Math.random() to generate nunber between 0 and 1
+console.log(Math.round(Math.random() * 10));
